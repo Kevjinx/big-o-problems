9a34d88268fca1e0e75d65e2e132111e3dd6a85e
Manna Kong <m.kong.57@gmail.com>
2021-05-19 15:11:15 -0500

    02 firstAnagram

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,5 +1,14 @@
 function firstAnagram(str1, str2) {
-  // Code goes here ....
+  let array1 = str1.split('');
+  let array2 = str2.split('');
+
+  while (array1.length > 0) {
+    if (!array2.indexOf(array1[0])) {
+      return false;
+    }
+    array1.shift();
+  }
+  return true;
 }
 
 
